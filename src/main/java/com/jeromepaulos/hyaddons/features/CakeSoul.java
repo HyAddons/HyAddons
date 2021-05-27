108fb1630e21597f3028d7ec158ad1dfd7372af5
Jerome Paulos <jeromepaulos@yahoo.com>
2021-05-27 12:53:59 -0700

    Show cake soul owner

---
+++ src/main/java/com/jeromepaulos/hyaddons/features/CakeSoul.java
@@ -0,0 +1,40 @@
+package com.jeromepaulos.hyaddons.features;
+
+import com.jeromepaulos.hyaddons.config.Config;
+import com.jeromepaulos.hyaddons.utils.Utils;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class CakeSoul {
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void onItemTooltip(ItemTooltipEvent event) {
+        if(Config.showCakeSoulOwner && Utils.inSkyBlock) {
+            ItemStack item = event.itemStack;
+            NBTTagCompound nbt = item.getTagCompound();
+
+            if(nbt.hasKey("ExtraAttributes")) {
+                String cakeOwner = nbt.getCompoundTag("ExtraAttributes").getString("cake_owner");
+                String capturedPlayer = nbt.getCompoundTag("ExtraAttributes").getString("captured_player");
+                String itemId = nbt.getCompoundTag("ExtraAttributes").getString("id");
+
+                if(itemId.equals("CAKE_SOUL")) {
+                    if(capturedPlayer.equals("")) {
+                        if(cakeOwner.equals("")) {
+                            event.toolTip.add(4, EnumChatFormatting.GRAY + "Owner: Not recorded");
+                        } else {
+                            event.toolTip.add(4, EnumChatFormatting.GRAY + "Owner: " + cakeOwner);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
